0c67b3de7b5f66a93ab176bda8933b699bf6f9d1
Marc Runkel <marc.runkel@plusforta.de>
2020-05-19 00:19:11 +0200

    First code checkin

---
+++ checkRepo.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: checkRepo <repoName>")
+		os.Exit(2)
+	}
+	repoName := os.Args[1]
+	token, found := os.LookupEnv("API_TOKEN")
+	if !found {
+		fmt.Println("Must set API_TOKEN environment variable")
+		os.Exit(2)
+	}
+
+	url := "https://gitea.pfdev.de/api/v1/repos/" + repoName
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", "token "+token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		os.Exit(1)
+	}
+}
